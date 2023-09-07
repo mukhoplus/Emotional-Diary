d5f5d94c97743b5bd0850c9951a749061d54fe1b
Mukho <mukho@kakao.com>
2023-09-07 13:18:34 +0900

    Update pages - Use path variable, query string, page moving

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,31 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지입니다.</p>
+      <button onClick={() => setSearchParams({ who: "mukho" })}>바꿔.</button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        go home
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        go back
+      </button>
     </div>
   );
 };
